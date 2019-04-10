988aec8bc40dcc6dfc348926c5bb64b734aec8f9
patrick <wyp4wj@gmail.com>
2019-04-10 10:20:18 -0700

    create-react-app robofriends

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render( < h1 > Hello, world < /h1>, document.getElementById('root'));
+
+        serviceWorker.unregister();
