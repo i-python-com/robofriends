15114d27ebb1bd32c36d33ec3af0d502d3fec0f5
patrick <wyp4wj@gmail.com>
2019-04-10 12:43:03 -0700

    create all component's father App, contain h1 SearchBox CardList

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
+// import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(<CardList robots={robots} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
